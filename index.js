09a5862be91939e77e19145e140981e0e946e159
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-19 14:18:18 -0700

    Index setup, need to define routes before full test

---
+++ index.js
@@ -0,0 +1,53 @@
+// Set Up Important Dependencies
+const path = require('path');
+
+// To be used as middleware
+const session = require('express-session');
+const express = require('express');
+
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Session cookie storage with sequelize
+const sess = {
+  secret: process.env.DB_SCRT,
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Middleware delaration
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Configure Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+// Initialize sync with database and start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
