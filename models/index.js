0a1681d9cbb9125ec0df5eaeca6eb0b0791af9c2
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-19 14:46:24 -0700

    Decided foreign keys related to comment (author will be among users table). Will create models and reference appropriate tables:

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// User and Post Relationships
 User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
@@ -10,5 +11,30 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+// Comment Relationships
 
-module.exports = { User, Post};
+// A comment is made by signed-in user per author_id
+Comment.belongsTo(User, {
+    foreignKey: 'author_id'
+});
+
+// A user can have many comments per author_id
+User.hasMany(Comment, {
+    foreignKey: 'author_id',
+});
+
+// Comments are also associated with a specific post_id
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+});
+
+// Posts can have many comments per post_id
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+
+
+module.exports = { User, Post, Comment};
