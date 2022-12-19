da18c81b649ef8a94097ab2452b8e7a535894295
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-19 14:37:59 -0700

    Object relationships

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require("./User");
 const Post = require("./Post");
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 
 module.exports = { User, Post};
