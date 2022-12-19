1b988ccb24e8181c966ebd7ffc8e068006c1e7f8
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-19 15:08:05 -0700

    Simplified post content

--- models/Post.js
+++ models/Post.js
@@ -15,9 +15,6 @@ Project.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
-      type: DataTypes.STRING,
-    },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
