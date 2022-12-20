44ec17ab3b6048e127ac92c2316735b2bc0aa7f5
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-19 18:51:49 -0700

    Format, create, and get routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,26 +3,62 @@ const withAuth = require("../../utils/auth");
 // Pull in model info from parent index file in models to descure depencencies' definitions
 const { Post, Comment } = require("../../models");
 
+// Create a New Post
 router.post("/", withAuth, async (req, res) => {
-  try {
-    // Get the info needed for the new post - use the user_id that is currently signed in through session
-    const postBody = {
-      title: req.body.name,
-      content: req.body.content,
-      date_created: Date.now(),
-      user_id: req.session.userID,
-    };
+    try {
+        // Get the info needed for the new post - use the user_id that is currently signed in through session
+        const postBody = {
+            title: req.body.name,
+            content: req.body.content,
+            date_created: Date.now(),
+            user_id: req.session.userID,
+        };
 
-    // Create the new post using the body
-    const newPost = await Post.create(postBody);
+        // Create the new post using the body
+        const newPost = await Post.create(postBody);
 
-    // Confirm if successfull
-    res.status(200).json(newPost);
-  } catch (err) {
-    // Log and send the error if not
-    console.log(err);
-    res.status(400).json(err);
-  }
+        // Confirm if successfull
+        res.status(200).json(newPost);
+    } catch (err) {
+        // Log and send the error if not
+        console.log(err);
+        res.status(400).json(err);
+    }
 });
+
+// Get all Posts (e.g. for front page)
+router.get("/", async (req, res) => {
+    try {
+        // Find all posts
+        const allPosts = await Post.findAll();
+
+        // Confirm if successfull
+        res.status(200).json(allPosts);
+    } catch (err) {
+        // Log and send the error if not
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+// Get One Post (when a user clicks on a post from home page or dashboard)
+router.get("/:id", async (req, res) => {
+    try {
+        // Find One Post
+        const singlePost = await Post.findOne({
+            where: {
+            id: req.params.id,
+            },
+        });
+
+        // Confirm if successfull
+        res.status(200).json(singlePost);
+    } catch (err) {
+        // Log and send the error if not
+        console.log(err);
+        res.status(400).json(err);
+    }
+  });
+
 
 module.exports = router;
