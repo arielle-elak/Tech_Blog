8147810023dd1cfa16488dfec2657f44c25e8206
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-19 19:50:00 -0700

    Comment create route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,27 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
+const router = require("express").Router();
+const { Comment } = require("../../models/index");
 
+// Make a New Comment
+router.post("/create", async (req, res) => {
+  try {
+    // Define comment body for logging purposes
+    const commentBody = {
+      author: req.session.username,
+      content: req.body.commentBody,
+      date_created: Date.now(),
+      author_id: req.session.userID,
+      post_id: req.body.postID,
+    };
 
-module.exports = router;
+    // Create the new comment using the above specified body content
+    const newComment = await Comment.create(commentBody);
+
+    // Confirm if successfull
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    // Log and send the error if not
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
