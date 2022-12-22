33c221d0b6d6055dc6e69406cc2d1f7223833a26
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-21 17:14:00 -0700

    Changed route to get, but still not able to find it

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -70,7 +70,7 @@ router.post("/login", async (req, res) => {
 
 // User Sign Out - if user is currently in logged_in state, delete the session entry
 // TODO: Find out why when /logout requested, tries to GET not POST
-router.post("/logout", (req, res) => {
+router.get("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
