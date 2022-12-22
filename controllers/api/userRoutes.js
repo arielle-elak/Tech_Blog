6a6b366ed4e7334d42dca46fa1b1fe98faeb4d55
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-21 17:14:52 -0700

    Isolated post is working since the session is being properly destroyed - it's the await that is not registering

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -69,14 +69,13 @@ router.post("/login", async (req, res) => {
 });
 
 // User Sign Out - if user is currently in logged_in state, delete the session entry
-// TODO: Find out why when /logout requested, tries to GET not POST
-router.get("/logout", (req, res) => {
+
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
-    // console.log("Can't find the logout!")
     res.status(404).end();
   }
 });
