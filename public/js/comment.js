810196bd3b73ab36ac0c186dcc940d36eea9085b
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-22 21:48:03 -0700

    Comment added, but page not refreshing

---
+++ public/js/comment.js
@@ -0,0 +1,37 @@
+// Create a new comment
+const commentSubmit = async (event) => {
+  // Prevent premature page reload
+  event.preventDefault();
+  console.log("commentSubmit event occured");
+
+  // Get comment information from page elements
+  const commentBody = document.querySelector("#comment-body").value.trim();
+  const postID = document
+    .querySelector("#comment-submit")
+    .getAttribute("data-post");
+  let success_flag = 0;
+  // console.log(postID)
+
+  //send comment to backend
+  if (commentBody) {
+    const response = await fetch(`/api/comments/create`, {
+      method: "POST",
+      body: JSON.stringify({ commentBody, postID }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      console.log("Comment added");
+      success_flag = 1;
+    } else {
+      alert("Failed to add comment");
+    }
+  }
+
+};
+
+document
+  .querySelector("#comment-submit")
+  .addEventListener("click", commentSubmit);
